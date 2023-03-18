4782db6668711809b289a02d88b6a4990af0b7af
tracer-ics2017 <tracer@njuics.org>
2023-03-18 09:38:18 +0800

    >  compile 2010237 root Linux premapect 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:38:18 up 40 min,  0 users,  load average: 0.04, 0.03, 0.00 f3f37d4aceafbb3fbff1606ba8ae98f9234a8a2d

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -73,7 +73,7 @@ WP* new_wp() {
 
 void free_wp(int N) {
   WP* victim = head;
-  while(victim->NO != N) {
+  while(victim->NO != N && victim) {
     victim = victim->next;
   }
   if(!victim) assert(0);
