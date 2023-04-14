78cb08232f7bb0a1acd46c9f102239b992ff866b
tracer-ics2017 <tracer@njuics.org>
2023-04-14 15:03:35 +0800

    >  compile 2010237 root Linux premapect 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:03:35 up  4:56,  0 users,  load average: 0.11, 0.07, 0.02 ad03d54be5c66a9273518fd729081ce12fbc0d8

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -32,7 +32,7 @@ make_EHelper(call)
   // eip入栈
   rtl_li(&t2, decoding.seq_eip);
   rtl_push(&t2);
-  decoding.is_jmp = true;
+  decoding.is_jmp = 1;
   print_asm("call %x", decoding.jmp_eip);
 }
 
@@ -40,7 +40,7 @@ make_EHelper(ret)
 {
   rtl_pop(&t2);
   decoding.jmp_eip = t2;
-  decoding.is_jmp = true;
+  decoding.is_jmp = 1;
   print_asm("ret");
 }
 
@@ -48,8 +48,7 @@ make_EHelper(call_rm)
 {
   rtl_li(&t2, decoding.seq_eip);
   rtl_push(&t2);
+  decoding.is_jmp = 1;
   decoding.jmp_eip = id_dest->val;
-  decoding.is_jmp = true;
-
   print_asm("call *%s", id_dest->str);
 }
