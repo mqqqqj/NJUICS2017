2720eb55784d767ddac6e75cc73247f120b22a57
tracer-ics2017 <tracer@njuics.org>
2023-04-14 18:47:45 +0800

    >  compile 2010237 root Linux premapect 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:47:45 up  8:40,  0 users,  load average: 0.02, 0.02, 0.00 51325f32b10b6d69a31cc756850f27a437764086

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -201,7 +201,7 @@ static inline void rtl_push(const rtlreg_t *src1)
 {
   // esp <- esp - 4
   // M[esp] <- src1
-  TODO();
+  // TODO();
   rtl_subi(&cpu.esp, &cpu.esp, 4);
   rtl_sm(&cpu.esp, 4, src1);
 }
