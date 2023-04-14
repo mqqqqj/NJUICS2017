e667687b1ffd4e0895470939055886911b415daf
tracer-ics2017 <tracer@njuics.org>
2023-04-14 19:35:55 +0800

    >  compile 2010237 root Linux premapect 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:35:55 up  9:28,  0 users,  load average: 0.09, 0.14, 0.10 45cfa4a7c4fcaf26a26d78b353fe18fac17964

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -59,12 +59,12 @@ make_EHelper(sal)
 
 make_EHelper(sar)
 {
-  TODO();
+  // TODO();
   // unnecessary to update CF and OF in NEMU
-  rtl_sext(&t3, &id_dest->val, id_dest->width);
-  rtl_sar(&t3, &t3, &id_src->val);
-  operand_write(id_dest, &t3);
-  rtl_update_ZFSF(&t3, id_dest->width);
+  rtl_sext(&t2, &id_dest->val, id_dest->width);
+  rtl_sar(&t2, &t2, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);
   print_asm_template2(sar);
 }
 
