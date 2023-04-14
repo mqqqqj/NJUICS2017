d412a1d2890de501e2656725e04149a059f3950b
tracer-ics2017 <tracer@njuics.org>
2023-04-14 11:12:25 +0800

    >  compile 2010237 root Linux premapect 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:12:25 up  1:05,  0 users,  load average: 0.02, 0.09, 0.04 434c6cb5c1da2c3d5ffd6f488e63958e30293208

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -33,7 +33,6 @@ make_EHelper(sub)
   // OF, SF, ZF, AF, PF, and CF are affected
   // t2 = dest - src
   rtl_sub(&t2, &id_dest->val, &id_src->val);
-
   // ZF,SF
   rtl_update_ZFSF(&t2, id_dest->width);
   // CF
